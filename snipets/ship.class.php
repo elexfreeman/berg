7fb1d59703e9ec5e3e693b8854c21a73f73d6261
elex <elextraza@gmail.com>
2015-09-27 15:29:26 +0300

    Класс шип

---
+++ snipets/ship.class.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elex
+ * Date: 27.09.15
+ * Time: 15:23
+ */
+
+
+function rus2translit($string) {
+    $converter = array(
+        'а' => 'a',   'б' => 'b',   'в' => 'v',
+        'г' => 'g',   'д' => 'd',   'е' => 'e',
+        'ё' => 'e',   'ж' => 'zh',  'з' => 'z',
+        'и' => 'i',   'й' => 'y',   'к' => 'k',
+        'л' => 'l',   'м' => 'm',   'н' => 'n',
+        'о' => 'o',   'п' => 'p',   'р' => 'r',
+        'с' => 'c',   'т' => 't',   'у' => 'u',
+        'ф' => 'f',   'х' => 'h',   'ц' => 'c',
+        'ч' => 'ch',  'ш' => 'sh',  'щ' => 'sch',
+        'ь' => '\'',  'ы' => 'y',   'ъ' => '\'',
+        'э' => 'e',   'ю' => 'yu',  'я' => 'ya',
+
+        'А' => 'A',   'Б' => 'B',   'В' => 'V',
+        'Г' => 'G',   'Д' => 'D',   'Е' => 'E',
+        'Ё' => 'E',   'Ж' => 'Zh',  'З' => 'Z',
+        'И' => 'I',   'Й' => 'Y',   'К' => 'K',
+        'Л' => 'L',   'М' => 'M',   'Н' => 'N',
+        'О' => 'O',   'П' => 'P',   'Р' => 'R',
+        'С' => 'C',   'Т' => 'T',   'У' => 'U',
+        'Ф' => 'F',   'Х' => 'H',   'Ц' => 'C',
+        'Ч' => 'Ch',  'Ш' => 'Sh',  'Щ' => 'Sch',
+        'Ь' => '_',  'Ы' => 'Y',   'Ъ' => '_',
+        'Э' => 'E',   'Ю' => 'Yu',  'Я' => 'Ya',
+    );
+    return strtr($string, $converter);
+}
+
+function encodestring($str) {
+    // переводим в транслит
+    $str = rus2translit($str);
+    // в нижний регистр
+    $str = strtolower($str);
+    // заменям все ненужное нам на "-"
+    $str = preg_replace('~[^-a-z0-9_]+~u', '-', $str);
+    // удаляем начальные и конечные '-'
+    $str = trim($str, "-");
+
+
+    return $str;
+}
+
+
+class Ship
+{
+    function GetPageInfo($product_id)
+    {
+        global $modx;
+        global $table_prefix;
+
+        $sql = "select * from " . $table_prefix . "site_content where id=" . $product_id;
+        foreach ($modx->query($sql) as $row) {
+            $product = new stdClass();
+            $product->id = $row['id'];
+            $product->introtext = $row['introtext'];
+            $product->description = $row['description'];
+            $product->title = $row['pagetitle'];
+            $product->url = $row['uri'];
+            //теперь дополнительные поля
+            // - 1 - если это подарки, то тут нету дополнительных цен
+            $tv = $this->GetContentTV($product_id);
+            $product->tv = $tv;
+
+        }
+        return $product;
+
+    }
+
+/*todo: сделать загрузку кораблей и круизо в модых*/
+}
